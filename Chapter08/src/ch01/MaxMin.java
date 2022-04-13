8816f0a0f0f2f610bc336ccd710be60db35d9410
KimTaekJun <86834898+Kuah0@users.noreply.github.com>
2022-04-13 15:20:40 +0900

    .

--- Chapter08/src/ch01/MaxMin.java
+++ Chapter08/src/ch01/MaxMin.java
@@ -1,25 +1,29 @@
-//package ch01;
-//
-//public class MaxMin {
-//    public static void main(String[] args) {
-//        int[] arr = {10, 55, 23, 2, 79, 101, 16, 82, 30, 45};
-//
-//        int Maxarr = arr[0];
-//        int Minarr = arr[0];
-//        int Max = 0;
-//        int Min = 0;
-//
-//        for (int i=1; i<arr.length; i++) {
-//            if (Max < Maxarr[i]) {
-//                Max = Maxarr[i];
-//                Maxarr = i+1;
-//            }
-//            if (Min > Minarr[i]) {
-//                Min = Minarr[i];
-//                Minarr = i+1;
-//            }
-//        }
-//        System.out.println("가장 큰 값은 " + Maxarr + " 이고, 위치는 " + Max + " 입니다." );
-//        System.out.println("가장 작은 값은 " + Minarr + " 이고, 위치는 " + Min + " 입니다." );
-//    }
-//}
+package ch01;
+
+public class MaxMin {
+    public static void main(String[] args) {
+        int[] arr = {10, 55, 23, 2, 79, 101, 16, 82, 30, 45};
+
+        int Min = arr[0];
+        int Max = arr[0];
+        int MinIndex = 0;
+        int MaxIndex = 0;
+
+        for(int i=1; i<arr.length; i++) {
+
+            if(Min > arr[i]) {
+                Min = arr[i];
+                MinIndex = i+1;
+            }
+
+            if(Max < arr[i]) {
+                Max = arr[i];
+                MaxIndex = i+1;
+            }
+        }
+
+        System.out.println("가장 큰 값은 " + Max + " 이고, 위치는 " + MaxIndex + "번 입니다.");
+        System.out.println("가장 작은 값은 " + Min + " 이고, 위치는 " + MinIndex + "번 입니다.");
+    }
+
+}
