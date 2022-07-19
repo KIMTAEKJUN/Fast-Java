5632230a74fa4c154dcfea684b20a40e3ab056c4
KimTaekJun <86834898+Kuah0@users.noreply.github.com>
2022-07-19 14:28:21 +0900

    수정

--- Chapter05/src/ch03/MyLinkedList.java
+++ Chapter05/src/ch03/MyLinkedList.java
@@ -1,5 +1,62 @@
 package ch03;
 
 public class MyLinkedList {
+    private MyListNode head;
+    int count;
 
+    public MyLinkedList() {
+        head = null;
+        count = 0;
+    }
+
+    public MyListNode addElement(String data) {
+        MyListNode newNode;
+        if (head == null) {
+            newNode = new MyListNode(data);
+            head = newNode;
+        } else {
+            newNode = new MyListNode(data);
+            MyListNode temp = newNode;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = newNode;
+        }
+        count++;
+        return newNode;
+    }
+
+    public MyListNode insertElement(String data, int position) {
+        int i;
+        MyListNode tempNode = head;
+        MyListNode preNode = null;
+
+        MyListNode newNode = new MyListNode(data);
+
+        if (position < 0 || position > count) {
+            return null;
+        }
+
+        if (position == 0) {
+            newNode.next = head;
+            head = newNode;
+        } else {
+            for (i=0; i<position; i++) {
+                preNode = tempNode;
+                tempNode = tempNode.next;
+            }
+            newNode.next = preNode.next;
+            preNode.next = newNode;
+        }
+        count++;
+        return newNode;
+    }
+
+    // public MyListNode removeElement(int position) {
+    //
+    // }
+
+    // public String getElement(int position) {
+    //
+    // }
 }
